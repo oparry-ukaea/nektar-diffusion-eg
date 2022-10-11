2d76999a04ab8004cbc05c1b1e0a14fcce196653
Owen Parry <owen.parry@ukaea.uk>
2022-10-11 14:11:40 +0100

    Modifiy script to generate BC files for the file-based_Lorenz template.

--- write_BC_files.py
+++ write_BC_files.py
@@ -3,15 +3,25 @@ import os
 import os.path
 import csv
 
+root_dir = os.path.dirname(os.path.realpath(__file__))
+
 #==================================================================================================
-def write_file(fpath,time,Omega=2.0):
-    T = math.sin(Omega*time)
-    header_vals = ["x","T"]
-    with open(fpath, 'w') as fh:
-        writer = csv.writer(fh)
-        writer.writerow(header_vals)
-        writer.writerow([0,T])
-        writer.writerow([1,T])
+def get_BCs(mode='lorenz', **mode_opts):
+    times = get_times()
+    temps = get_temps(times, mode=mode, **mode_opts)
+    return zip(times,temps)
+#==================================================================================================
+
+#==================================================================================================
+def get_temps(times, mode='lorenz', **mode_opts):
+    if mode=='lorenz':
+        T = read_lorenz_data(npoints=len(times))
+    elif mode=='sin':
+        Omega = mode_opts.get('Omega',2.0)
+        T = [math.sin(Omega*time) for time in times]
+    else:
+        exit(f"write_file: '{mode}' is not a valid mode")
+    return T
 #==================================================================================================
 
 #==================================================================================================
@@ -25,12 +35,40 @@ def get_times(dstep = 5e-3,nsteps = 1000,substeps = [0, 2.18e-3, 3.59e-3]):
 #==================================================================================================
 
 #==================================================================================================
-def main(template="file-based"):
-    root_dir = os.path.dirname(os.path.realpath(__file__))
-    for time in get_times():
+def read_lorenz_data(npoints,nstride=1,r=28.0):
+    data_fpath = os.path.join(root_dir,"data","orig","lorenz.csv")
+    data = []
+    with open(data_fpath) as fh:
+        _ = fh.readline() # skip header
+        for ii in range(npoints):
+            line = fh.readline()
+            if ii % nstride == 0:
+                data.append(1+2*float(line.split(",")[2])/r )
+    return data
+#==================================================================================================
+
+#==================================================================================================
+def write_file(fpath,time, T):
+
+    header_vals = ["x","T"]
+    with open(fpath, 'w') as fh:
+        writer = csv.writer(fh)
+        writer.writerow(header_vals)
+        writer.writerow([0,T])
+        writer.writerow([1,T])
+#==================================================================================================
+
+
+#==================================================================================================
+def main(mode="lorenz"):
+
+    template_dir = os.path.join(root_dir,"runs","templates",f"file-based_{mode}")
+    mode_opts = {}
+    
+    for time, T in get_BCs(mode=mode, **mode_opts):
         fname = "BCVals_{:5.2E}.csv".format(time)
-        fpath = os.path.join(root_dir,"runs","templates",template,fname)
-        write_file(fpath,time)
+        fpath = os.path.join(template_dir,fname)
+        write_file(fpath,time,T)
 #==================================================================================================
 
 main()
