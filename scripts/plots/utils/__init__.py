37c2c34e32deb7f53a0ede0dce0a523fb21dc9d3
Owen Parry <owen.parry@ukaea.uk>
2022-11-17 12:57:21 +0000

    Add some scripts to plot and animate 2D fields.

--- scripts/plots/utils/__init__.py
+++ scripts/plots/utils/__init__.py
@@ -1,4 +1,4 @@
 from .locations import get_plot_path, get_run_root, get_template_root
-from .plotting import get_plot_style
+from .plotting import get_plot_style, plot_2d_field, animate_2d_field
 from .sin_analytic import calc_analytic
-from .srcs import read_all_nektar_srcs
+from .srcs import read_all_nektar_srcs, read_nektar_src
