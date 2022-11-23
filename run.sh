a20ae49a655bf7a9335a47f57a9d02ccc5644104
Owen Parry <owen.parry@ukaea.uk>
2022-11-23 11:51:20 +0000

    Add a 2D template - simple, non-coupled BCs for now.

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # Defaults - run in debug and output to a subdirectory tagged with the current date and time
 MODE="RELEASE"
-TEMPLATE_SUBDIR="file-based_lorenz"
+TEMPLATE_SUBDIR="2d-diff"
 # Parse command line args
 for arg in $*; do
     case "$arg" in
--- scripts/plots/utils/srcs.py
+++ scripts/plots/utils/srcs.py
@@ -14,7 +14,7 @@ def read_all_nektar_srcs(run_dir, chk_start=0,chk_end=99,chk_stride=1,compute_gr
     for chk_num in range(chk_start,chk_end+chk_stride,chk_stride):
         nek_src = get_source("nektar", run_dir, chk_num=chk_num, label=f"Checkpoint {chk_num}")
         params =  nek_src.get_session().GetParameters()
-        nek_src.label = "t = {:.2f}".format(params["TIMESTEP"]*params["IO_CHECKSTEPS"]*(chk_num+1))
+        nek_src.label = "t = {:.2f}".format(params["TIMESTEP"]*params["IO_CHECKSTEPS"]*(chk_num))
         if compute_gradients:
             nek_src.add_gradients()
         nek_src.set_plot_kws(get_plot_style(plot_style))
